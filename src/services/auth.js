fcf0432816ac20ecd2d100aca46d07e79b37390e
appswifts <appswifts@gmail.com>
2026-05-23 20:16:02 +0200

    Send login as form-urlencoded (backend rejects JSON)

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,12 @@
 import request from './request';
 
 const authService = {
-  login: (data) => request.post('auth/login', data),
-  logout: (data) => request.post('auth/logout', data),
+  login: (data) => request.post('auth/login', new URLSearchParams(data), {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  }),
+  logout: (data) => request.post('auth/logout', new URLSearchParams(data), {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  }),
 };
 
 export default authService;
